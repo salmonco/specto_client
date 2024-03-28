89c506a35b7c102a9d901d343b1aa41ae779afd9
salmonco <xkqjsslsek80@naver.com>
2024-03-28 10:44:52 +0900

    Design: 회고_회고 추가하기_완료 #6

--- src/screens/ReviewAddComplete.tsx
+++ src/screens/ReviewAddComplete.tsx
@@ -1,11 +1,31 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { SafeAreaView, View } from "react-native";
+import { CustomText as Text } from "@components/CustomText";
+import Tack from "@assets/images/tack.svg";
+import Button from "@components/Button";
 
 function ReviewAddComplete() {
   return (
-    <View>
-      <Text>ReviewAddComplete</Text>
-    </View>
+    <SafeAreaView className="flex-1">
+      <View className="flex-1 justify-between py-[26] pb-[44] px-[20]">
+        <Text
+          className="font-[Inter-Medium] text-[#373737] text-center"
+          size={13}
+        >
+          2024.03.28
+        </Text>
+        <View className="items-center">
+          <Tack />
+          <Text className="font-[Inter-Medium] text-[#373737] text-center mt-[40]">
+            회고가 저장되었어요.{"\n"}오늘도 수고 많으셨습니다 :{")"}
+          </Text>
+        </View>
+        <Button
+          label="홈으로"
+          callbackFn={() => console.log("홈으로 이동 버튼을 눌렀습니다.")}
+        />
+      </View>
+    </SafeAreaView>
   );
 }
 
