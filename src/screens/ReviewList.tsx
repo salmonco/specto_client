68a3270fc2dd651a880fc2dd603f1f9a7c6388d1
salmonco <xkqjsslsek80@naver.com>
2024-03-29 15:48:08 +0900

    Design: 회고_회고리스트_UI #8

--- src/screens/ReviewList.tsx
+++ src/screens/ReviewList.tsx
@@ -1,10 +1,65 @@
-import React from "react";
-import { Text, View } from "react-native";
+import React, { useState } from "react";
+import { FlatList, Pressable, View } from "react-native";
+import { CustomText as Text } from "@components/CustomText";
+import SpecListItem, {
+  SPEC_DATA,
+  SPEC_MENU,
+  SpecBase,
+} from "@components/SpecListItem";
 
 function ReviewList() {
+  const [clickedCategory, setClickedCategory] = useState("all");
+  const [specList, setSpecList] = useState(SPEC_DATA);
+
+  const renderItem = ({ item }: { item: SpecBase }) => {
+    return <SpecListItem item={item} />;
+  };
+
   return (
-    <View>
-      <Text>ReviewList</Text>
+    <View className="flex-1">
+      <View className="flex-row justify-between gap-[8] py-[23] px-[20] border-b border-b-[#ECEBEB]">
+        {SPEC_MENU.map((v) => (
+          <Pressable
+            className={`w-[52] h-[38] justify-center items-center ${
+              v.category === clickedCategory ? "bg-[#0094FF]" : "bg-[#EAF4FF]"
+            }`}
+            style={{ borderRadius: 4 }}
+            key={v.category}
+            onPress={() => setClickedCategory(v.category)}
+          >
+            <Text
+              className={`font-[Inter-Medium] text-center ${
+                v.category === clickedCategory ? "text-white" : "text-[#0094FF]"
+              }`}
+              size={12}
+            >
+              {v.label}
+            </Text>
+          </Pressable>
+        ))}
+      </View>
+      <FlatList
+        contentContainerStyle={{
+          gap: 15,
+          paddingVertical: 20,
+          paddingHorizontal: 20,
+        }}
+        data={specList}
+        renderItem={renderItem}
+        keyExtractor={(item) => `${item.id}`}
+        showsVerticalScrollIndicator={false}
+        ListHeaderComponent={
+          <View className="flex-row items-center justify-end mr-[15]">
+            <Text
+              className="text-[#1C1C1E] font-[Inter-Medium] mr-[5]"
+              size={12}
+            >
+              최근 등록순
+            </Text>
+            <View className="w-0 h-0 border-l-4 border-l-transparent border-r-4 border-r-transparent border-b-4 border-b-[#1C1C1E] rotate-180" />
+          </View>
+        }
+      />
     </View>
   );
 }
