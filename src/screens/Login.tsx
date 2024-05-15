e27a9632c20c89b54f05b3cd44e91cf64f42d4e6
salmonco <xkqjsslsek80@naver.com>
2024-05-15 19:35:06 +0900

    Chore: 환경변수

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -14,6 +14,7 @@ import Logo from "../../assets/images/logo.svg";
 import axios from "axios";
 import { NativeStackScreenProps } from "@react-navigation/native-stack";
 import { AuthStackParamList } from "@stackNav/Auth";
+import getEnvVars from "environment";
 const VIEW_HEIGHT = Dimensions.get("window").height; // 화면 세로길이
 
 type AuthProps = NativeStackScreenProps<AuthStackParamList, "Login">;
@@ -21,7 +22,7 @@ type AuthProps = NativeStackScreenProps<AuthStackParamList, "Login">;
 function Login({ navigation }: Readonly<AuthProps>) {
   const kakaoLogin = async () => {
     try {
-      const res = await axios.get("http://13.210.239.98:8080/login/kakao");
+      const res = await axios.get(`${getEnvVars()?.apiUrl}/login/kakao`);
       const redirectUrl = res.request.responseURL; // responseURL 추출
       console.log(redirectUrl);
       if (redirectUrl) {
