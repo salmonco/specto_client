2379bb111eae97cfa485b5a605fe2b194ce32c6f
salmonco <xkqjsslsek80@naver.com>
2024-05-16 11:04:47 +0900

    Chore: 액세스토큰 남아 있는 거로 자동 로그인 판단

--- AppInner.tsx
+++ AppInner.tsx
@@ -1,15 +1,14 @@
 import * as React from "react";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import { useSelector } from "react-redux";
-import { RootState } from "./src/store/reducer";
 import Splash from "./src/screens/Splash";
 import Auth from "./src/stackNav/Auth";
 import Main from "./src/tabNav/Main";
 import { useAxiosInterceptor } from "src/hooks/useAxiosInterceptor";
+import * as SecureStore from "expo-secure-store";
 
 export type RootStackParamList = {
   Splash: undefined;
-  Auth: { screen: "Login" };
+  Auth: undefined;
   Main: undefined;
   // SpecDetail: { id: number }; // id 매개변수 추가
 };
@@ -17,14 +16,25 @@ export type RootStackParamList = {
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 function AppInner() {
-  // const dispatch = useAppDispatch();
-  const isLoggedIn = useSelector((state: RootState) => !!state.user.name);
+  const [isLoggedIn, setIsLoggedIn] = React.useState(false);
+  console.log("isLoggedIn", isLoggedIn);
   useAxiosInterceptor();
+
+  React.useEffect(() => {
+    const checkIsLoggedIn = async () => {
+      const accessToken = await SecureStore.getItemAsync("accessToken");
+      setIsLoggedIn(!!accessToken);
+    };
+    checkIsLoggedIn();
+  }, []);
 
   return (
     <Stack.Navigator screenOptions={{ headerShown: false }}>
       {isLoggedIn ? (
-        <Stack.Screen name="Main" component={Main} />
+        <Stack.Group>
+          <Stack.Screen name="Main" component={Main} />
+          <Stack.Screen name="Auth" component={Auth} />
+        </Stack.Group>
       ) : (
         <Stack.Group>
           <Stack.Screen name="Splash" component={Splash} />
